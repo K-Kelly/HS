3e748d8391988e4c04b6a5aae3a66458de297ffd
K Kelly <kevinskelly@wustl.edu>
2012-05-31 00:50:32 -0500

    Update to Django v1.4, Bootstrap v2.0.3, add registration tests

--- hockey/tests.py
+++ hockey/tests.py
@@ -5,7 +5,8 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 from datetime import datetime
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase
+from selenium import webdriver
 from wuska.hockey.models import Player, Team, League, Arena
 class PlayerTest(TestCase):
     
@@ -43,8 +44,58 @@ class PlayerTest(TestCase):
         pants = 1
         skates = 1
         stick = 1
-        self.player = Player.objects.create(team_id = team_id, user_id = user_id, upgrades = upgrades, level = level, experience = experience, name = name, age = age, retired = retired, height = height, weight = weight, salary =salary, contract_end = contract_end, no_trade = no_trade, position = position, style = style, shooting = shooting, passing = passing, stickHandling = stickHandling, checking = checking, positioning = positioning, endurance = endurance, skating = skating, strength = strength, faceoff = faceoff, fighting = fighting, awareness = awareness, leadership = leadership, helmet = helmet, gloves = gloves, shoulder_pads = shoulder_pads, pants = pants, skates = skates, stick = stick)
+        free_agent = True
+        self.player = Player.objects.create(team_id = team_id, user_id = user_id, upgrades = upgrades, level = level, experience = experience, name = name, age = age, retired = retired, height = height, weight = weight, salary =salary, contract_end = contract_end, no_trade = no_trade, position = position, style = style, shooting = shooting, passing = passing, stickHandling = stickHandling, checking = checking, positioning = positioning, endurance = endurance, skating = skating, strength = strength, faceoff = faceoff, fighting = fighting, awareness = awareness, leadership = leadership, helmet = helmet, gloves = gloves, shoulder_pads = shoulder_pads, pants = pants, skates = skates, stick = stick, free_agent = free_agent)
 
     def test_player(self):
         self.assertEqual(self.player.skates, 100)
         self.assertEqual(self.player.name, "hello")
+
+class PlayerTest(LiveServerTestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_cant_login_via_login_page(self):
+        self.browser.get(self.live_server_url + '/accounts/login/')
+        username_box = self.browser.find_element_by_id('id_username')
+        password_box = self.browser.find_element_by_id('id_password')
+        self.assertIn('',username_box.text)
+        self.assertIn('',password_box.text)
+        self.browser.find_element_by_id('submit').click()
+        self.assertIn('/accounts/login', self.browser.current_url)
+        
+    def test_can_register_new_user(self):
+        self.browser.get(self.live_server_url + '/accounts/register/')
+        username_box = self.browser.find_element_by_id('id_username')
+        email_box = self.browser.find_element_by_id('id_email')
+        password1_box = self.browser.find_element_by_id('id_password1')
+        password2_box = self.browser.find_element_by_id('id_password2')
+        self.assertIn('',username_box.text)
+        self.assertIn('',email_box.text)
+        self.assertIn('',password1_box.text)
+        self.assertIn('',password2_box.text)
+        self.browser.find_element_by_id('submit').click()
+        self.assertIn('/accounts/register', self.browser.current_url)
+
+        self.browser.get(self.live_server_url + '/accounts/register/')
+        username_box = self.browser.find_element_by_id('id_username').send_keys('Test')
+        email_box = self.browser.find_element_by_id('id_email')
+        email_box.send_keys('testuser@localhostadfsdfsf')
+        self.browser.find_element_by_id('submit').click()
+        self.assertIn('/accounts/register', self.browser.current_url)
+        email_box = self.browser.find_element_by_id('id_email')
+        email_box.send_keys('.com')
+        password1_box = self.browser.find_element_by_id('id_password1')
+        password1_box.send_keys('testing1')
+        password2_box = self.browser.find_element_by_id('id_password2')
+        password2_box.send_keys('ExFail')
+        self.browser.find_element_by_id('submit').click()
+        self.assertIn('/accounts/register', self.browser.current_url)
+        
+        
+        
+        
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from wuska import settings
 admin.autodiscover()
 
@@ -33,4 +34,6 @@ urlpatterns += patterns('',
 )
 
 urlpatterns += patterns('',
-                        (r'^assets/(?P<path>.*)$', 'django.views.static.serve',{'document_root':     settings.MEDIA_ROOT}),)
+                        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),)
+
+urlpatterns += staticfiles_urlpatterns()
